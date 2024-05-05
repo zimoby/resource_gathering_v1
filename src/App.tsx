3bc5d326762daaa51d6d49aafbf9d736155ad960
Denys Bondartsov <zimoby@gmail.com>
2024-05-05 15:36:38 +0300

    refacatoring and impruvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState, useCallback } from "react";
+import { useRef, useEffect, useState, useCallback, Key } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import {
   DoubleSide,
@@ -11,50 +11,29 @@ import {
 } from "three";
 import { createNoise2D } from "simplex-noise";
 import seedrandom from "seedrandom";
-import { OrbitControls, Stats } from "@react-three/drei";
+import { Cylinder, OrbitControls, OrthographicCamera, Sphere, Stats } from "@react-three/drei";
 import { useControls } from "leva";
-import useStore from "./store";
+import useStore, { resourceTypes, terrainTypes } from "./store";
 
-const terrainTypes = {
-  water: {
-    color: new Color(0x0000ff), // blue
-    level: -9,
-  },
-  grass: {
-    color: new Color(0x008000), // green
-    level: 0,
-  },
-  dirt: {
-    color: new Color(0xa52a2a), // brown
-    level: 5,
-  },
-  snow: {
-    color: new Color(0xffffff), // white
-    level: 10,
-  },
-  default: {
-    color: new Color(0x8b4513), // brown
-  },
+const getChunkCoordinates = (globalX: number, globalY: number, chunkSize: number) => {
+  const chunkX = Math.floor(globalX / chunkSize);
+  const chunkY = Math.floor(globalY / chunkSize);
+  return { chunkX, chunkY };
 };
 
-const resourceTypes = {
-  r1: {
-    color: new Color(0xffffff), // white
-  },
-  r2: {
-    color: new Color(0xffa500), // orange
-  },
-  r3: {
-    color: new Color(0x800080), // purple
-  },
-  r4: {
-    color: new Color(0xff4080), // pink
-  },
+const useKeyboardControls = ({
+  direction,
+  customSpeed,
+}: {
+  direction: {
+    current: { x: number; y: number; }; x: number; y: number 
 };
-
-const useKeyboardControls = ({direction, customSpeed}) => {
+  customSpeed: {
+    current: number;
+  };
+}): void => {
   useEffect(() => {
-    const handleKeyDown = (event) => {
+    const handleKeyDown = (event: { key: any }) => {
       switch (event.key) {
         case "ArrowUp":
         case "w":
@@ -75,12 +54,20 @@ const useKeyboardControls = ({direction, customSpeed}) => {
         case "Shift":
           customSpeed.current = 3;
           break;
+        case " ":
+          useStore.setState({ canPlaceBeacon: true });
+          break;
       }
     };
 
-    const handleKeyUp = (event) => {
-      if (event.key === "Shift") {
-        customSpeed.current = 1;
+    const handleKeyUp = (event: { key: any }) => {
+      switch (event.key) {
+        case "Shift":
+          customSpeed.current = 1;
+          break;
+        case " ":
+          useStore.setState({ canPlaceBeacon: false });
+          break;
       }
     };
 
@@ -92,32 +79,118 @@ const useKeyboardControls = ({direction, customSpeed}) => {
       window.removeEventListener("keyup", handleKeyUp);
     };
   }, []);
+
+  // return { canPlaceBeacon, setCanPlaceBeacon };
 };
 
-const useCanvasClick = ({camera, raycaster, handleObjectClick, meshRef}) => {
+const useCanvasClick = ({
+  camera,
+  raycaster,
+  handleObjectClick,
+  meshRef,
+  resources,
+  offsetX,
+  offsetY,
+}) => {
+  const canPlaceBeacon = useStore((state) => state.canPlaceBeacon);
+
   useEffect(() => {
-    const handleCanvasClick = (event) => {
+    const handleCanvasClick = (event: { clientX: number; clientY: number }) => {
+      if (!canPlaceBeacon) return;
+
       const mouse = new Vector2(
         (event.clientX / window.innerWidth) * 2 - 1,
         -(event.clientY / window.innerHeight) * 2 + 1
       );
-  
+
       raycaster.setFromCamera(mouse, camera);
       const intersects = raycaster.intersectObject(meshRef.current);
-  
+
       if (intersects.length > 0) {
-        handleObjectClick(intersects[0]);
+        handleObjectClick(
+          {
+            point: intersects[0].point,
+            face: intersects[0].face,
+            resources,
+          },
+          offsetX,
+          offsetY
+        );
       }
     };
-  
+
     window.addEventListener("click", handleCanvasClick);
     return () => {
       window.removeEventListener("click", handleCanvasClick);
     };
-  }, [camera, handleObjectClick, meshRef]);
+  }, [camera, meshRef, canPlaceBeacon, offsetX, offsetY]);
+  // }, [camera, handleObjectClick, meshRef, canPlaceBeacon, raycaster, resources, offsetX, offsetY]);
 };
 
-const checkResource = (height) => {
+const addBeacon = (
+  position: { x: number; z: number; y: number },
+  resource: any,
+  offsetX: number,
+  offsetY: number
+) => {
+  const currentChunk = useStore.getState().currentLocation;
+  const beacons = useStore.getState().beacons;
+  const chunkBeacons = beacons.filter(
+    (beacon: { chunk: { x: any; y: any } }) =>
+      beacon.chunk.x === currentChunk.x && beacon.chunk.y === currentChunk.y
+  );
+
+  const minDistance = 10;
+
+  const isWithinRadius = chunkBeacons.some((beacon: { position: { x: number; z: number } }) => {
+    const dx = position.x - offsetX - beacon.position.x;
+    const dz = position.z - offsetY - beacon.position.z;
+    const distance = Math.sqrt(dx * dx + dz * dz);
+    return distance < minDistance;
+  });
+
+  if (isWithinRadius) {
+    useStore.setState({ message: "Cannot place beacon too close to another beacon." });
+    return;
+  }
+
+  // console.log("Beacons in chunk:", [beacon.chunk.x, beacon.chunk.y, currentChunk.x, currentChunk.y]);
+
+  if (chunkBeacons.length >= 2) {
+    useStore.setState({ message: "Maximum beacons placed in this chunk." });
+    // console.log("Maximum beacons placed in this chunk.");
+    return;
+  }
+
+  useStore.setState((state: { beacons: any }) => {
+    const newBeacons = [
+      ...state.beacons,
+      {
+        position: {
+          x: position.x.toFixed(3) - offsetX,
+          y: position.y.toFixed(3),
+          z: position.z.toFixed(3) - offsetY,
+        },
+        resource,
+        chunk: currentChunk,
+        visible: true,
+      },
+    ];
+    return { beacons: newBeacons };
+  });
+};
+
+const handleObjectClick = ({ point, face, resources }: any, offsetX: any, offsetY: any) => {
+  if (face) {
+    const vertexIndex = face.a;
+    const selectedResource = resources.current[vertexIndex];
+    useStore.setState({ selectedResource: selectedResource });
+
+    addBeacon(point, selectedResource, offsetX, offsetY);
+  }
+};
+
+const checkResource = (height: number) => {
   for (const [type, { level }] of Object.entries(terrainTypes)) {
     if (height < level) {
       return type;
@@ -126,11 +199,17 @@ const checkResource = (height) => {
   return "default";
 };
 
-const getResourceColor = (resourceType) => {
+const getResourceColor = (resourceType: string | number) => {
   return resourceTypes[resourceType]?.color || new Color(0xffffff);
 };
 
-const applyTerrainColors = (positions, colors, widthCount, depthCount, scale) => {
+const applyTerrainColors = (
+  positions: any[] | Float32Array,
+  colors: any[] | Float32Array,
+  widthCount: number,
+  depthCount: number,
+  scale: number
+) => {
   for (let i = 0; i <= depthCount; i++) {
     for (let j = 0; j <= widthCount; j++) {
       const idx = (i * (widthCount + 1) + j) * 3;
@@ -148,11 +227,8 @@ const applyTerrainColors = (positions, colors, widthCount, depthCount, scale) =>
 
 const applyResources = ({ resources, widthCount, depthCount, scale, offsetX, offsetY, rng }) => {
   const noise2D = createNoise2D(rng);
-  const resourceScale = scale * 0.021 ;
+  const resourceScale = scale * 0.021;
   const speedFactor = resourceScale / 100;
-  
-  const resourceTypes = ['r1', 'r2', 'r3', 'r4'];
-  const thresholds = [0.1, 0.2, 0.4, 1];
 
   for (let i = 0; i <= depthCount; i++) {
     for (let j = 0; j <= widthCount; j++) {
@@ -160,9 +236,9 @@ const applyResources = ({ resources, widthCount, depthCount, scale, offsetX, off
       const y = (i / depthCount) * resourceScale + offsetY * speedFactor;
       const noiseValue = (noise2D(x, y) + 1) / 2;
 
-      for (let k = 0; k < thresholds.length; k++) {
-        if (noiseValue < thresholds[k]) {
-          resources[i * (widthCount + 1) + j] = resourceTypes[k];
+      for (const [type, { threshold }] of Object.entries(resourceTypes)) {
+        if (noiseValue < threshold) {
+          resources[i * (widthCount + 1) + j] = type;
           break;
         }
       }
@@ -217,7 +293,7 @@ const generateTerrain = (
   applyTerrainColors(positions, colors, widthCount, depthCount, scale);
 
   const resources = new Array((widthCount + 1) * (depthCount + 1)).fill(null);
-  applyResources({resources, widthCount, depthCount, scale, offsetX, offsetY, rng});
+  applyResources({ resources, widthCount, depthCount, scale, offsetX, offsetY, rng });
 
   if (showResources) {
     for (let i = 0; i < resources.length; i++) {
@@ -239,6 +315,21 @@ const generateTerrain = (
   return { colors, resources };
 };
 
+const isOutOfBound = (
+  position: { x: any; y?: any; z: any },
+  width: number,
+  depth: number,
+  offsetX: number,
+  offsetY: number
+) => {
+  return (
+    position.x < -width / 2 + offsetX ||
+    position.x > width / 2 + offsetX ||
+    position.z < -depth / 2 + offsetY ||
+    position.z > depth / 2 + offsetY
+  );
+};
+
 const Terrain = () => {
   const { width, depth, resolution, scale, seed, offsetX, offsetY } = useControls({
     width: { value: 100, min: 50, max: 200 },
@@ -251,49 +342,70 @@ const Terrain = () => {
   });
 
   const { speed } = useControls({
-    speed: { value: 0.1, min: 0.01, max: 0.5 },
+    speed: { value: 0.1, min: 0, max: 0.5 },
   });
 
   const { camera } = useThree();
 
   const showResources = useStore((state) => state.showResources);
+  const beacons = useStore((state) => state.beacons);
+  // const selectResource = useStore((state) => state.selectRecource);
   const meshRef = useRef();
   const terrainGeometry = useRef(new BufferGeometry());
   const offset = useRef({ x: 0, y: 0 });
   const direction = useRef({ x: 0, y: -1 });
   const customSpeed = useRef(1);
-  const [selectedPoint, setSelectedPoint] = useState(null);
+  // const [selectedResource, setSelectedResource] = useState(null);
   // const [resources, setResources] = useState([]);
   const resources = useRef([]);
 
   const raycaster = new Raycaster();
 
-  const handleObjectClick = useCallback(({ point, face }) => {
-    if (face) {
-      const vertexIndex = face.a;
-      console.log("Resource:", { resources: resources.current, vertexIndex }, resources.current[vertexIndex]);
-    }
-  }, []);
+  useKeyboardControls({ direction, customSpeed });
 
-  useKeyboardControls({direction, customSpeed})
-  useCanvasClick({camera, raycaster, handleObjectClick, meshRef});
+  useKeyboardControls({
+    direction,
+    customSpeed,
+  });
+
+  useCanvasClick({
+    camera,
+    raycaster,
+    handleObjectClick,
+    meshRef,
+    resources,
+    offsetX,
+    offsetY,
+  });
 
   useEffect(() => {
     const { resources: generatedResources } = generateTerrain(
-      width, depth, resolution, scale, seed, offsetX, offsetY, terrainGeometry.current, showResources
+      width,
+      depth,
+      resolution,
+      scale,
+      seed,
+      offsetX,
+      offsetY,
+      terrainGeometry.current,
+      showResources
     );
     resources.current = generatedResources;
   }, [width, depth, resolution, scale, seed, offsetX, offsetY, showResources]);
 
   const updateTerrainGeometry = () => {
     const { colors, resources: generatedResources } = generateTerrain(
-      width, depth, resolution, scale, seed,
+      width,
+      depth,
+      resolution,
+      scale,
+      seed,
       offset.current.x + offsetX,
       offset.current.y + offsetY,
       terrainGeometry.current,
       showResources
     );
-    terrainGeometry.current.setAttribute('color', new Float32BufferAttribute(colors, 3));
+    terrainGeometry.current.setAttribute("color", new Float32BufferAttribute(colors, 3));
     resources.current = generatedResources;
 
     if (meshRef.current) {
@@ -301,32 +413,153 @@ const Terrain = () => {
     }
   };
 
-  useFrame(() => {
-    offset.current.x += direction.current.x * (speed * customSpeed.current);
-    offset.current.y += direction.current.y * (speed * customSpeed.current);
-  
-    updateTerrainGeometry();
+  const updateBeacons = (deltaX: number, deltaY: number) => {
+    const updatedBeacons = beacons
+      .map((beacon: { position: { x: any; y: any; z: any }; visible: boolean }) => {
+        const newPosition = {
+          x: parseFloat((beacon.position.x - deltaX).toFixed(2)),
+          y: beacon.position.y,
+          z: parseFloat((beacon.position.z - deltaY).toFixed(2)),
+        };
 
+        // Check bounds here if necessary
+        beacon.visible = !isOutOfBound(newPosition, width, depth, offsetX, offsetY);
+        beacon.position = newPosition;
+
+        return beacon;
+      })
+      .filter(Boolean);
+
+    useStore.setState({ beacons: updatedBeacons });
+  };
+
+  useFrame(() => {
+    const deltaX = direction.current.x * (speed * customSpeed.current);
+    const deltaY = direction.current.y * (speed * customSpeed.current);
+
+    offset.current.x += deltaX;
+    offset.current.y += deltaY;
+
+    // console.log("global position:", getChunkCoordinates(offset.current.x + offsetX + 100, offset.current.y + offsetY + 100, width).chunkY);
+    const currentChunk = getChunkCoordinates(
+      offset.current.x + offsetX + width / 2,
+      offset.current.y + offsetY + depth / 2,
+      width
+    );
+    useStore.setState({ currentLocation: { x: currentChunk.chunkX, y: currentChunk.chunkY } });
+    updateTerrainGeometry();
+    updateBeacons(deltaX, deltaY);
   });
 
   return (
-    <mesh ref={meshRef} geometry={terrainGeometry.current} >
+    <mesh ref={meshRef} geometry={terrainGeometry.current}>
       <meshStandardMaterial wireframe={true} vertexColors side={DoubleSide} />
     </mesh>
   );
 };
 
-const App = () => {
-  const toggleShowResources = useStore((state) => state.toggleShowResources); 
+const Beacons = () => {
+  const beacons = useStore((state) => state.beacons);
+  const { offsetX, offsetY, width, depth } = useControls({
+    offsetX: { value: 0, min: -100, max: 100 },
+    offsetY: { value: 0, min: -100, max: 100 },
+    width: { value: 100, min: 50, max: 200 },
+    depth: { value: 100, min: 50, max: 200 },
+  });
+
+  const beaconHeight = 10;
 
   return (
     <>
+      {beacons.map(
+        (
+          beacon: { position: { x: any; y: any; z: any }; visible: any },
+          index: string | number | bigint | undefined
+        ) => {
+          const position = {
+            x: beacon.position.x,
+            y: beacon.position.y,
+            z: beacon.position.z,
+          };
+
+          const isVisible =
+            beacon.visible && !isOutOfBound(position, width, depth, offsetX, offsetY);
+
+          return (
+            isVisible && (
+              <group
+                key={index}
+                position={[position.x - offsetX, position.y, position.z - offsetY]}
+              >
+                <Sphere args={[1, 8, 8]} position={[0, beaconHeight, 0]} />
+                <Cylinder args={[0.1, 0.1, beaconHeight, 4]} position={[0, beaconHeight / 2, 0]} />
+              </group>
+            )
+          );
+        }
+      )}
+    </>
+  );
+};
+
+const App = () => {
+  const toggleShowResources = useStore((state) => state.toggleShowResources);
+  const selectedResource = useStore((state) => state.selectedResource);
+  const currentLocation = useStore((state) => state.currentLocation);
+  const beacons = useStore((state) => state.beacons);
+  const playerPoints = useStore((state) => state.playerPoints);
+  const message = useStore((state) => state.message);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // console.log("Interval:", beacons);
+      useStore.setState((state: { playerPoints: any; beacons: any[] }) => ({
+        playerPoints:
+          state.playerPoints +
+          state.beacons.reduce(
+            (total: any, beacon: { resource: string | number }) =>
+              total + resourceTypes[beacon.resource].score,
+            0
+          ),
+      }));
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className=" w-72 fixed bottom-0 right-0">
+        {beacons.map(
+          (
+            beacon: { chunk: { x: any; y: any }; resource: any; position: { x: any; z: any } },
+            index: Key | null | undefined
+          ) => (
+            <div key={index}>
+              {`CH${beacon.chunk.x}${beacon.chunk.y}: ${beacon.resource}: ${beacon.position.x}, ${beacon.position.z}`}
+              {/* {beacon.position.x}, {beacon.position.z} */}
+            </div>
+          )
+        )}
+      </div>
+      {/* <div className="fixed left-1/2"> */}
       <button onClick={toggleShowResources}>Toggle Resource View</button>
+      {/* </div> */}
+      <div className="flex fixed bottom-0 left-0 flex-col">
+        <div>Selected Resource: {selectedResource}</div>
+        <div>Current Location: {JSON.stringify(currentLocation)}</div>
+        <div className=" text-lg ">Player Points: {playerPoints}</div>
+      </div>
+      <div className="fixed bottom-0 left-1/2">{message}</div>
       <Stats />
-      <Canvas flat shadows camera={{ position: [200, 200, 200], fov: 25 }}>
+      <Canvas flat shadows>
         <ambientLight intensity={0.5} />
+        <OrthographicCamera makeDefault position={[100, 100, 100]} zoom={7} />
         <directionalLight position={[10, 10, 5]} intensity={2} castShadow />
         <Terrain />
+        <Beacons />
         <OrbitControls />
       </Canvas>
     </>
--- src/store.tsx
+++ src/store.tsx
@@ -1,9 +1,65 @@
 // store.js
-import create from 'zustand';
+import { Color } from "three";
+import create from "zustand";
 
-const useStore = create(set => ({
+export const terrainTypes = {
+  water: {
+    color: new Color(0x0000ff), // blue
+    level: -9,
+  },
+  grass: {
+    color: new Color(0x008000), // green
+    level: 0,
+  },
+  dirt: {
+    color: new Color(0xa52a2a), // brown
+    level: 5,
+  },
+  snow: {
+    color: new Color(0xffffff), // white
+    level: 10,
+  },
+  default: {
+    color: new Color(0x8b4513), // brown
+  },
+};
+
+export const resourceTypes = {
+  r1: {
+    color: new Color(0xffffff), // white
+    threshold: 0.1,
+    score: 10,
+  },
+  r2: {
+    color: new Color(0xffa500), // orange
+    threshold: 0.2,
+    score: 7,
+  },
+  r3: {
+    color: new Color(0x800080), // purple
+    threshold: 0.4,
+    score: 5,
+  },
+  r4: {
+    color: new Color(0xff4080), // pink
+    threshold: 1,
+    score: 1,
+  },
+};
+
+const useStore = create((set) => ({
   showResources: false,
-  toggleShowResources: () => set(state => ({ showResources: !state.showResources }))
+  selectedResource: "",
+  currentLocation: { x: 0, y: 0 },
+  beacons: [],
+  playerPoints: 0,
+  message: "",
+
+	canPlaceBeacon: false,
+
+  toggleShowResources: () => set((state) => ({ showResources: !state.showResources })),
+
+  // selectResource: (resource) => set({ selectedResource: resource }),
 }));
 
 export default useStore;
