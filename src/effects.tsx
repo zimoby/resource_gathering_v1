896723a26929a65b452eefd1645ffbf8617f9152
Denys Bondartsov <zimoby@gmail.com>
2024-05-06 16:29:01 +0300

    refactoring

---
+++ src/effects.tsx
@@ -0,0 +1,39 @@
+import {
+  EffectComposer,
+  Bloom,
+  ChromaticAberration,
+  Depth,
+  DepthOfField,
+  Noise,
+  Scanline,
+  Sepia,
+  SMAA,
+  FXAA,
+  HueSaturation,
+} from "@react-three/postprocessing";
+
+export const EffectsCollection = () => {
+  return (
+    <EffectComposer>
+      {/* <FXAA /> */}
+      <HueSaturation hue={0} saturation={0.6} />
+      <Bloom
+        // blendFunction={AdditiveBlending}
+        mipmapBlur={true}
+        // radius={0.3}
+        intensity={3}
+        // radius={0}
+        // luminanceThreshold={0.9}
+        // luminanceSmoothing={1}
+        // height={500}
+      />
+      <ChromaticAberration offset={[0, 0.003]} radialModulation={true} />
+      <Noise opacity={0.2} />
+
+      {/* <Scanline
+                      density={0.7}
+                      opacity={0.2}
+                    /> */}
+    </EffectComposer>
+  );
+};
