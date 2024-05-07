969ed84c6c9d0cc2ff5a60f853de9c5d8472ecf8
Denys Bondartsov <zimoby@gmail.com>
2024-05-08 01:22:18 +0300

    fix

--- src/functions/paramsSync.tsx
+++ src/functions/paramsSync.tsx
@@ -5,23 +5,21 @@ import useGamaStore from "../store";
 export const useParamsSync = () => {
   // const mapParams = useGamaStore((state) => state.mapParams);
 
-  const { width, depth, resolution, scale, seed, speed } = useControls({
+  const { width, depth, resolution, scale, seed, offsetX, offsetY, speed } = useControls({
     width: { value: 200, min: 1, max: 200 },
     depth: { value: 200, min: 1, max: 200 },
     resolution: { value: 3, min: 1, max: 10 },
     scale: { value: 50, min: 10, max: 100 },
     seed: "42",
-    // offsetX: { value: 0, min: -100, max: 100 },
-    // offsetY: { value: 0, min: -100, max: 100 },
+    offsetX: { value: 0, min: -100, max: 100 },
+    offsetY: { value: 0, min: -100, max: 100 },
 		speed: { value: 0.1, min: 0, max: 0.5 },
   });
 
   useEffect(() => {
-		useGamaStore.setState({ mapParams: { width, depth, resolution, scale, seed, speed } });
-	}, [width, depth, resolution, scale, seed, speed]);
-  // useEffect(() => {
-	// 	useGamaStore.setState({ mapParams: { width, depth, resolution, scale, seed, offsetX, offsetY, speed } });
-	// }, [width, depth, resolution, scale, seed, offsetX, offsetY, speed]);
+		useGamaStore.setState({ mapParams: { width, depth, resolution, scale, seed, offsetX, offsetY, speed } });
+		// console.log("mapPgame store updated");
+	}, [width, depth, resolution, scale, seed, offsetX, offsetY, speed]);
 
 	// useEffect(() => {
 	// 	levaStore.set({ ...mapParams });
