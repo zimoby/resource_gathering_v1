e0ac624a1d11f8d7c5c74451e1618beab0022654
Denys Bondartsov <zimoby@gmail.com>
2024-05-09 23:05:37 +0300

    add rulers

---
+++ src/components/LinearGridShader.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useMemo, useRef } from "react";
+import { DoubleSide, Color, Vector2, PlaneGeometry, ShaderMaterial } from "three";
+import useGamaStore from "../store";
+
+// import { vertexShader, fragmentShader } from './chunkGridShader';
+
+const vertexShader = `
+  varying vec2 vUv;
+
+  void main() {
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  }
+`;
+
+const fragmentShader = `
+  uniform float chunkSize;
+  uniform vec2 offset;
+	uniform float subGridsX;
+	uniform float subGridsY;
+  uniform float lineWidth;
+  uniform vec3 gridColor;
+  uniform vec3 subGridColor;
+	uniform vec2 gridSize;
+  
+  varying vec2 vUv;
+
+	void main() {
+    vec2 coord = (vUv + offset / chunkSize) * chunkSize;
+    
+    // Calculate edge fading with a sharp cutoff based on lineWidth
+    float edgeFadeX = step(lineWidth / chunkSize, min(coord.x, gridSize.x - coord.x));
+    float edgeFadeY = step(lineWidth / chunkSize, min(coord.y, gridSize.y - coord.y));
+    float edgeFade = edgeFadeY;
+
+    vec2 subGrid = abs(fract(vec2(coord.x * subGridsX, coord.y * subGridsY) - 0.5) - 0.5) / fwidth(vec2(coord.x * subGridsX, coord.y * subGridsY));
+    float subLine = min(subGrid.x, subGrid.y);
+
+    float subAlpha = (1.0 - min(subLine, 1.0)) * edgeFade;
+
+    vec3 color = mix(vec3(0.0), subGridColor, step(lineWidth, subLine));
+
+    gl_FragColor = vec4(color, subAlpha * 0.5);
+}
+`;
+
+export const LinearGridShader = ({position = [0,0,0], sizeX = 100, sizeY = 100, width = 100, depth = 100}) => {
+	// const { width: mapWidth, depth: mapDepth, } = useGamaStore((state) => state.mapParams);
+
+  const gridConfig = useGamaStore((state) => state.gridConfig);
+	const rulerRef = useRef();
+
+  useEffect(() => {
+    generateRulerGeometry();
+  }, [width, depth, sizeX, sizeY, gridConfig]);
+
+  const generateRulerGeometry = useMemo(() => {
+    const planeGeometry = new PlaneGeometry(sizeX, sizeY, 1, 1);
+		const planeMaterial = new ShaderMaterial({
+			uniforms: {
+					chunkSize: { value: gridConfig.chunkSize },
+					offset: { value: new Vector2(0, 0) },
+					subGridsX: { value: width }, // Adjusted to handle X axis
+					subGridsY: { value: depth }, // Adjusted to handle Y axis
+					lineWidth: { value: gridConfig.lineWidth },
+					gridColor: { value: new Color(gridConfig.subGridColor) },
+					subGridColor: { value: new Color(gridConfig.subGridColor) },
+					gridSize: { value: new Vector2(sizeX, sizeY) },
+			},
+			vertexShader,
+			fragmentShader,
+			transparent: true,
+			side: DoubleSide,
+			depthWrite: false,
+	});
+
+    return () => {
+      rulerRef.current.geometry = planeGeometry;
+      rulerRef.current.material = planeMaterial;
+    };
+  }, [sizeX, sizeY, gridConfig.chunkSize, gridConfig.lineWidth, gridConfig.gridColor, gridConfig.subGridColor, width, depth]);
+
+  return (
+    <mesh position={position} ref={rulerRef} rotation={[-Math.PI / 2, 0, 0]}>
+      <planeGeometry />
+      <shaderMaterial />
+    </mesh>
+  );
+};
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,19 +2,38 @@ import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import useGamaStore from "../store";
 import { getChunkCoordinates, useCalculateDeltas, useUpdateMapMoving } from "../functions/functions";
-import { BasicGridShader } from "./BasicGridShader";
 import { BeaconGroup } from "./beacons/BeaconGroup";
 import { Terrain } from "./terrain/Terrain";
+import { LinearGridShader } from "./LinearGridShader";
+import { BasicGridShader } from "./BasicGridShader";
+import { useControls } from "leva";
+
 
 export const Map = () => {
   const firstStart = useGamaStore((state) => state.firstStart);
   const planeRef = useRef();
   const offset = useRef({ x: 0, y: 0 });
+  const { width } = useGamaStore((state) => state.mapParams);
+
+  const { rulerGridX, rulerGridY, sizeX, sizeY } = useControls({
+    // sizeX: {
+    //   value: 100, min: 1, max: 300, step: 1,
+    // },
+    // sizeY: {
+    //   value: 100, min: 1, max: 300, step: 1,
+    // },
+    // rulerGridX: {
+    //   value: 50, min: 1, max: 100, step: 1,
+    // },
+    rulerGridY: {
+      value: 50, min: 1, max: 100, step: 1,
+    },
+  });
 
   const { deltaX, deltaY } = useCalculateDeltas();
   const { updateLocationAndOffset } = useUpdateMapMoving();
 
-  console.log("Map rendering");
+  // console.log("Map rendering");
   
   useFrame(() => {
     offset.current.x += deltaX;
@@ -29,6 +48,26 @@ export const Map = () => {
     <group visible={firstStart}>
       <BeaconGroup />
       <Terrain />
+      {/* <TerrainVertex /> */}
+      <group>
+        <group position={[0,0,width / 2 + 4]}>
+          <LinearGridShader position={[0,0,-1]} sizeX={width} sizeY={2} width={rulerGridY} depth={1} />
+          <LinearGridShader position={[0,0,0]} sizeX={width} sizeY={5} width={rulerGridY / 2} depth={1} />
+        </group>
+        <group position={[0,0,-width / 2 - 4]} rotation-x={Math.PI}>
+          <LinearGridShader position={[0,0,-1]} sizeX={width} sizeY={2} width={rulerGridY} depth={1} />
+          <LinearGridShader position={[0,0,0]} sizeX={width} sizeY={5} width={rulerGridY / 2} depth={1} />
+        </group>
+        <group position={[width / 2 + 4,0,0]} rotation-y={Math.PI / 2}>
+          <LinearGridShader position={[0,0,-1]} sizeX={width} sizeY={2} width={rulerGridY} depth={1} />
+          <LinearGridShader position={[0,0,0]} sizeX={width} sizeY={5} width={rulerGridY / 2} depth={1} />
+        </group>
+        <group position={[-width / 2 - 4,0,0]} rotation-y={-Math.PI / 2}>
+          <LinearGridShader position={[0,0,-1]} sizeX={width} sizeY={2} width={rulerGridY} depth={1} />
+          <LinearGridShader position={[0,0,0]} sizeX={width} sizeY={5} width={rulerGridY / 2} depth={1} />
+        </group>
+      </group>
+
       <BasicGridShader planeRef={planeRef} />
     </group>
   );
