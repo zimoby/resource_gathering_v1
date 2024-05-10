596e47c855ee98b01d50c4feac2a43d8e0ce907c
Denys Bondartsov <zimoby@gmail.com>
2024-05-10 13:02:11 +0300

    update ui info

--- src/components/uiInfo.tsx
+++ src/components/uiInfo.tsx
@@ -5,6 +5,9 @@ import useGamaStore from "../store";
 export const UiInfo = () => {
   const selectedResource = useGamaStore((state) => state.selectedResource);
   const selectedChunk = useGamaStore((state) => state.selectedChunk);
+  const mapParams = useGamaStore((state) => state.mapParams);
+  const logs = useGamaStore((state) => state.logs);
+  // const weatherCondition = useGamaStore((state) => state.weatherCondition);
 
   const message = useGamaStore((state) => state.message);
 
@@ -20,7 +23,17 @@ export const UiInfo = () => {
   
   return (
     <>
-      <div className="orbitron z-50 fixed top-0 left-0 text-6xl">PLANET-01</div>
+      <div className="fixed z-50 top-0 left-0">
+        <div className="flex flex-col">
+          <div className="orbitron z-50 text-6xl">{`PLANET-${mapParams.seed}`}</div>
+          <div className="scrollbar z-50 p-1 h-fit max-h-56 w-52 text-left m-2 text-xs rounded-sm border border-white/80">
+            LOGS:
+            {logs.map((log, index) => (
+              <div key={index}>{log}</div>
+            ))}
+          </div>
+        </div>
+      </div>
       <div className="z-50 flex fixed bottom-0 left-0 flex-col">
         <div>Selected Chunk: {convertChunkCoordinateToName(selectedChunk)}</div>
         <div>Selected Resource: {selectedResource}</div>
