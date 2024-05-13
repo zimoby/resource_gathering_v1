ec085932d5e3956a424222c519d3f8808b19fe98
Denys Bondartsov <zimoby@gmail.com>
2024-05-14 01:41:49 +0300

    refactor

--- src/Scene/startScreen.tsx
+++ src/Scene/startScreen.tsx
@@ -18,7 +18,10 @@ const StartScreen = () => {
   const [userActive, setUserActive] = useState(false);
   const [skipStartScene, setSkipStartScene] = useState(false);
   const [starting, setStarting] = useState(false);
-	const [sounds, setSounds] = useState<{ ambient: Howl | null, click: Howl | null }>({ ambient: null, click: null });
+  const [sounds, setSounds] = useState<{ ambient: Howl | null; click: Howl | null }>({
+    ambient: null,
+    click: null,
+  });
 
   useEffect(() => {
     const handleUserGesture = () => {
@@ -62,7 +65,7 @@ const StartScreen = () => {
       if (sounds.ambient) sounds.ambient.unload();
       if (sounds.click) sounds.click.unload();
     };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [userActive]);
 
   useEffect(() => {
@@ -78,44 +81,89 @@ const StartScreen = () => {
     sounds.click?.play();
     setTimeout(() => {
       setStartScreen("startScreen", false);
-			if (startScreen === skipStartScene) {
-				updateVariableInLocalStorage("startScreen", !skipStartScene)
-			}
+      if (startScreen === skipStartScene) {
+        updateVariableInLocalStorage("startScreen", !skipStartScene);
+      }
     }, 1000);
   };
 
   return (
     <div className="h-full w-full bg-black">
-      <div className={`h-full w-full flex flex-col justify-center items-center bg-neutral-900 ${starting ? "animate-fadeOut" : ""}`}>
+      <div
+        className={`h-full w-full flex flex-col justify-center items-center bg-neutral-900 ${
+          starting ? "animate-fadeOut" : ""
+        }`}
+      >
         <div className="w-20 -mb-1 h-auto">
           <div className="w-full h-full flex justify-center items-center">{corpLogoSvg}</div>
         </div>
-        <h1 className="orbitron text-7xl uppercase text-center text-yellow-400" style={{ transform: "perspective(40px) rotateX(5deg)" }}>
+        <h1
+          className="orbitron text-7xl uppercase text-center text-yellow-400"
+          style={{ transform: "perspective(40px) rotateX(5deg)" }}
+        >
           World researcher
         </h1>
-        <h1 className="orbitron text-xs mt-3 uppercase -skew-x-12">Project for the Threejs.journey course</h1>
+        <h1 className="orbitron text-xs mt-3 uppercase -skew-x-12">
+          Project for the Threejs.journey course
+        </h1>
         <p className="text-center w-3/5 orbitron text-2xs mt-3 uppercase -skew-x-12 scale-y-150 scale-x-75 leading-3">
           {`Course by Bruno Simon Design by ${authorName} story by ${authorName} animation by ${authorName} development by ${authorName} testing by ${authorName} vfx by ${authorName}`}
         </p>
 
-        {loadingProgress < 100 && (
+        {!userActive && (
+          <div className="w-fit h-fit mt-16 flex flex-row items-center justify-center border border-neutral-100 hover:border-yellow-400 overflow-hidden bg-neutral-100 hover:bg-yellow-400 cursor-pointer">
+            {/* <div className="w-36 h-full overflow-hidden">
+						<div className="w-72 h-full flex flex-col justify-center items-center bg-repeat-x animate-linear" style={{ background: "repeating-linear-gradient(-45deg, transparent, transparent 10px, black 10px, black 20px)" }} />
+					</div> */}
+            <button className="w-32 m-2 uppercase text-center text-neutral-900">
+              Generate World
+            </button>
+            {/* <div className="w-36 h-full overflow-hidden">
+						<div className="w-72 h-full flex flex-col justify-center items-center bg-repeat-x animate-linear" style={{ background: "repeating-linear-gradient(-45deg, transparent, transparent 10px, black 10px, black 20px)" }} />
+					</div> */}
+          </div>
+        )}
+
+        {userActive && loadingProgress < 100 && (
           <div className="w-[300px] h-10 mt-16 bg-neutral-800 border">
-            <div className="h-full overflow-hidden bg-neutral-200" style={{ width: `${loadingProgress * 3}px` }}>
-              <div className="h-full flex flex-col justify-center items-center bg-repeat-x animate-linear" style={{ width: `${loadingProgress * 6}px`, background: "repeating-linear-gradient(-45deg, transparent, transparent 10px, black 10px, black 20px)" }} />
+            <div
+              className="h-full overflow-hidden bg-neutral-200"
+              style={{ width: `${loadingProgress * 3}px` }}
+            >
+              <div
+                className="h-full flex flex-col justify-center items-center bg-repeat-x animate-linear"
+                style={{
+                  width: `${loadingProgress * 6}px`,
+                  background:
+                    "repeating-linear-gradient(-45deg, transparent, transparent 10px, black 10px, black 20px)",
+                }}
+              />
             </div>
           </div>
         )}
 
-        {loadingProgress === 100 && (
+        {userActive && loadingProgress === 100 && (
           <div className="w-fit h-fit mt-16 flex flex-row items-center justify-center border border-neutral-100 hover:border-yellow-400 overflow-hidden bg-neutral-100 hover:bg-yellow-400 cursor-pointer">
             <div className="w-36 h-full overflow-hidden">
-              <div className="w-72 h-full flex flex-col justify-center items-center bg-repeat-x animate-linear" style={{ background: "repeating-linear-gradient(-45deg, transparent, transparent 10px, black 10px, black 20px)" }} />
+              <div
+                className="w-72 h-full flex flex-col justify-center items-center bg-repeat-x animate-linear"
+                style={{
+                  background:
+                    "repeating-linear-gradient(-45deg, transparent, transparent 10px, black 10px, black 20px)",
+                }}
+              />
             </div>
             <button className="w-32 m-2 uppercase text-center text-neutral-900" onClick={startGame}>
               Start Game
             </button>
             <div className="w-36 h-full overflow-hidden">
-              <div className="w-72 h-full flex flex-col justify-center items-center bg-repeat-x animate-linear" style={{ background: "repeating-linear-gradient(-45deg, transparent, transparent 10px, black 10px, black 20px)" }} />
+              <div
+                className="w-72 h-full flex flex-col justify-center items-center bg-repeat-x animate-linear"
+                style={{
+                  background:
+                    "repeating-linear-gradient(-45deg, transparent, transparent 10px, black 10px, black 20px)",
+                }}
+              />
             </div>
           </div>
         )}
@@ -123,12 +171,25 @@ const StartScreen = () => {
         <div className="h-fit mt-16 flex flex-col justify-center items-center">
           <p>Settings</p>
           <p className="w-3/5 text-xs text-center leading-3">
-            The game includes glitch effects that may cause discomfort or seizures for people with photosensitive epilepsy. If you find these effects uncomfortable you can disable them.
+            The game includes glitch effects that may cause discomfort or seizures for people with
+            photosensitive epilepsy. If you find these effects uncomfortable you can disable them.
           </p>
           <div className="mt-2 flex flex-row gap-3">
-            <ToggleButton text={"Animations"} onClick={() => updateVariableInLocalStorage("disableAnimations", !disableAnimations)} state={disableAnimations} />
-            <ToggleButton text={"Sound"} onClick={() => updateVariableInLocalStorage("disableSounds", !disableSounds)} state={disableSounds} />
-            <ToggleButton text={"Skip start scene"} onClick={() => setSkipStartScene(!skipStartScene)} state={skipStartScene} />
+            <ToggleButton
+              text={"Animations"}
+              onClick={() => updateVariableInLocalStorage("disableAnimations", !disableAnimations)}
+              state={disableAnimations}
+            />
+            <ToggleButton
+              text={"Sound"}
+              onClick={() => updateVariableInLocalStorage("disableSounds", !disableSounds)}
+              state={disableSounds}
+            />
+            <ToggleButton
+              text={"Skip start scene"}
+              onClick={() => setSkipStartScene(!skipStartScene)}
+              state={skipStartScene}
+            />
           </div>
         </div>
       </div>
