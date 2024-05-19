af83536860f32da00e7518412ad48ab46b59e972
Denys Bondartsov <zimoby@gmail.com>
2024-05-19 17:39:10 +0300

    adding after element

--- src/components/UI/BasicPanelWrapper.tsx
+++ src/components/UI/BasicPanelWrapper.tsx
@@ -12,8 +12,8 @@ export const BasicPanelWrapper = ({
   
   return (
     <div
-      className={` ${height} ${width} overflow-hidden text-left text-xs border border-uilines`}
-      style={{ opacity: opacity }}
+    className={` ${height} ${width} relative text-left text-xs border border-uilines after:overflow-hidden after:content after:block after:w-4 after:h-4 after:border-t after:border-r after:bg-transparent after:absolute after:-bottom-1 after:-left-1 after:z-10`}     
+     style={{ opacity: opacity }}
     >
       <p className="w-full h-fit px-1 bg-uilines text-neutral-900 select-none">{titleText}</p>
       <div className="scrollbar w-full h-full p-1 text-uitext">{children}</div>
