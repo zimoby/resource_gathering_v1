083ff8f63fc67245eec0cfe96e3e8d601059baad
Denys Bondartsov <zimoby@gmail.com>
2024-05-23 06:10:20 +0300

    add few keys processing

--- src/hooks/intereaction.tsx
+++ src/hooks/intereaction.tsx
@@ -1,10 +1,10 @@
-import { RefObject, useCallback, useEffect, useRef } from "react";
+import { RefObject, useCallback, useEffect, useRef, useState } from "react";
 import { Camera, Mesh, Raycaster, Vector2 } from "three";
 import { useGameStore } from "../store/store";
 import { ResourceType } from "../store/worldParamsSlice";
 import { debounce, throttle } from "lodash";
 import { useProcessBeacons } from "../components/beacons/beaconUtils";
-import { getChunkCoordinates } from "../utils/functions";
+import { getChunkCoordinates, useCheckVariableRender } from "../utils/functions";
 import { useProcessArtefacts } from "../components/artefacts/artefactUtils";
 
 const getIntersection = (
@@ -27,6 +27,45 @@ const getIntersection = (
   }
 };
 
+// const useMultiKeyControls = () => {
+//   const [activeKeys, setActiveKeys] = useState(new Set());
+
+//   useEffect(() => {
+//       const handleKeyDown = (event) => {
+//           setActiveKeys(prev => new Set(prev.add(event.key)));
+//       };
+
+//       const handleKeyUp = (event) => {
+//           setActiveKeys(prev => {
+//               const newKeys = new Set(prev);
+//               newKeys.delete(event.key);
+//               return newKeys;
+//           });
+//       };
+
+//       window.addEventListener('keydown', handleKeyDown);
+//       window.addEventListener('keyup', handleKeyUp);
+
+//       return () => {
+//           window.removeEventListener('keydown', handleKeyDown);
+//           window.removeEventListener('keyup', handleKeyUp);
+//       };
+//   }, []);
+
+//   return activeKeys;
+// };
+
+const keyToVector: { [key: string]: { x: number; y: number } } = {
+  'ArrowUp': { x: 0, y: -1 },
+  'KeyW': { x: 0, y: -1 },
+  'ArrowDown': { x: 0, y: 1 },
+  'KeyS': { x: 0, y: 1 },
+  'ArrowLeft': { x: -1, y: 0 },
+  'KeyA': { x: -1, y: 0 },
+  'ArrowRight': { x: 1, y: 0 },
+  'KeyD': { x: 1, y: 0 }
+};
+
 export const useKeyboardControls = ({
   meshRef,
   camera,
@@ -38,6 +77,9 @@ export const useKeyboardControls = ({
 }): void => {
   const canPlaceBeacon = useGameStore((state) => state.canPlaceBeacon);
   const mouseEventRef = useRef<MouseEvent | null>(null);
+  const [activeKeys, setActiveKeys] = useState({});
+
+  // useCheckVariableRender(activeKeys, "activeKeys");
 
   const handleMousePosition = useCallback((event: MouseEvent) => {
     mouseEventRef.current = event;
@@ -45,6 +87,11 @@ export const useKeyboardControls = ({
 
   const handleKeyDown = useCallback(
     (event: KeyboardEvent) => {
+      
+      if (keyToVector[event.code]) {
+        setActiveKeys((prev) => ({ ...prev, [event.code]: true }));
+      }
+    
       const intersects = getIntersection(
         mouseEventRef.current || { clientX: 0, clientY: 0 },
         raycaster,
@@ -52,27 +99,12 @@ export const useKeyboardControls = ({
         camera
       );
 
-      switch (event.key) {
-        case "ArrowUp":
-        case "w":
-          useGameStore.setState({ moveDirection: { x: 0, y: -1 } });
-          break;
-        case "ArrowDown":
-        case "s":
-          useGameStore.setState({ moveDirection: { x: 0, y: 1 } });
-          break;
-        case "ArrowLeft":
-        case "a":
-          useGameStore.setState({ moveDirection: { x: -1, y: 0 } });
-          break;
-        case "ArrowRight":
-        case "d":
-          useGameStore.setState({ moveDirection: { x: 1, y: 0 } });
-          break;
-        case "Shift":
+      switch (event.code) {
+        case "ShiftLeft":
+        case "ShiftRight":
           useGameStore.setState({ dynamicSpeed: 3 });
           break;
-        case " ":
+        case "Space":
           useGameStore.setState({ canPlaceBeacon: true });
 
           if (intersects.length > 0 && !canPlaceBeacon) {
@@ -91,15 +123,38 @@ export const useKeyboardControls = ({
   );
 
   const handleKeyUp = useCallback((event: KeyboardEvent) => {
-    switch (event.key) {
-      case "Shift":
+
+    setActiveKeys((prev) => {
+      const newKeys: { [key: string]: boolean } = { ...prev };
+      delete newKeys[event.code];
+      return newKeys;
+    });
+
+    switch (event.code) {
+      case "ShiftLeft":
+      case "ShiftRight":
         useGameStore.setState({ dynamicSpeed: 1 });
         break;
-      case " ":
+      case "Space":
         useGameStore.setState({ canPlaceBeacon: false });
         break;
     }
   }, []);
+
+  useEffect(() => {
+    const moveDirection = Object.entries(activeKeys).reduce((acc, [key, active]) => {
+      if (active && keyToVector[key]) {
+        acc.x += keyToVector[key].x;
+        acc.y += keyToVector[key].y;
+      }
+      return acc;
+    }, { x: 0, y: 0 });
+
+    if (moveDirection.x !== 0 || moveDirection.y !== 0) {
+      // console.log("moveDirection", moveDirection);
+      useGameStore.setState({ moveDirection });
+    }
+  }, [activeKeys]);
 
   useEffect(() => {
     window.addEventListener("keydown", handleKeyDown);
@@ -129,14 +184,20 @@ export const useCanvasHover = ({
   const { width, depth, offsetX, offsetY } = useGameStore((state) => state.mapParams);
   const { addBeacon } = useProcessBeacons();
   const { takeArtefact, checkArtefactInRadius } = useProcessArtefacts();
+  const addNewMessage = useGameStore((state) => state.addNewMessage);
+  const costs = useGameStore((state) => state.costs);
+  const playerPoints = useGameStore((state) => state.playerPoints);
 
-  const throttledSetState = throttle((state) => {
+  const throttledSetState = useRef(throttle((state) => {
     useGameStore.setState(state);
-  }, 100);
+  }, 100)).current;
 
   const handleCanvasHover = useCallback(
     (event: { clientX: number; clientY: number; type: string }) => {
       if (!canPlaceBeacon || !meshRef.current) {
+        if(playerPoints < costs.scanning.value) {
+          addNewMessage("Not enough energy to scan");
+        }
         return;
       }
 
@@ -147,67 +208,56 @@ export const useCanvasHover = ({
         camera
       );
 
-      if (intersects.length > 0) {
-        const { point, face } = intersects[0];
-        if (!face) return;
-
-        const vertexIndex = face.a;
-        const resource = resources.current[vertexIndex];
-
-        debounce(() => {
-          useGameStore.setState({
-            activePosition: point,
-          });
-        }, 100)();
-
-        const { currentOffset } = useGameStore.getState();
-
-        const currentPosition = {
-          x: point.x + width / 2 + currentOffset.x,
-          y: point.z + depth / 2 + currentOffset.y,
-        };
-
-        throttledSetState({
-          selectedResource: resource,
-          selectedChunk: getChunkCoordinates(currentPosition.x, currentPosition.y, width),
-        });
-
-        const isWithinRadius = checkArtefactInRadius({ point });
-
-        if (event.type === "click") {
-          if (isWithinRadius) {
-            takeArtefact({ artefactId: isWithinRadius.id });
-            return;
-          }
-
-          addBeacon({
-            position: {
-              x: point.x - offsetX,
-              y: point.y,
-              z: point.z - offsetY,
-            },
-            resource,
-            currentChunk: getChunkCoordinates(currentPosition.x, currentPosition.y, width),
-          });
-          // useGameStore.setState({ canPlaceBeacon: false });
-        }
+      if (intersects.length === 0) {
+        return;
       }
+
+      const { point, face } = intersects[0];
+      if (!face) return;
+
+      const vertexIndex = face.a;
+      const resource = resources.current[vertexIndex];
+
+      debounce(() => {
+        useGameStore.setState({
+          activePosition: point,
+        });
+      }, 100)();
+
+      const { currentOffset } = useGameStore.getState();
+
+      const currentPosition = {
+        x: point.x + width / 2 + currentOffset.x,
+        y: point.z + depth / 2 + currentOffset.y,
+      };
+
+      throttledSetState({
+        selectedResource: resource,
+        selectedChunk: getChunkCoordinates(currentPosition.x, currentPosition.y, width),
+      });
+
+      const isWithinRadius = checkArtefactInRadius({ point });
+
+      if (event.type === "click") {
+        if (isWithinRadius) {
+          takeArtefact({ artefactId: isWithinRadius.id });
+          return;
+        }
+
+        addBeacon({
+          position: {
+            x: point.x - offsetX,
+            y: point.y,
+            z: point.z - offsetY,
+          },
+          resource,
+          currentChunk: getChunkCoordinates(currentPosition.x, currentPosition.y, width),
+        });
+        // useGameStore.setState({ canPlaceBeacon: false });
+      }
+
     },
-    [
-      canPlaceBeacon,
-      meshRef,
-      raycaster,
-      camera,
-      resources,
-      width,
-      depth,
-      throttledSetState,
-      checkArtefactInRadius,
-      addBeacon,
-      offsetX,
-      offsetY,
-      takeArtefact,
-    ]
+    [canPlaceBeacon, meshRef, raycaster, camera, playerPoints, costs.scanning.value, addNewMessage, resources, width, depth, throttledSetState, checkArtefactInRadius, addBeacon, offsetX, offsetY, takeArtefact]
   );
 
   useEffect(() => {
