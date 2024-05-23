2f3fbaefac234abc1606ba663e740ca71fda3082
Denys Bondartsov <zimoby@gmail.com>
2024-05-23 03:17:48 +0300

    refactoring

--- src/components/gfx/LinearGridShader1.tsx
+++ src/components/gfx/LinearGridShader1.tsx
@@ -20,7 +20,6 @@ const fragmentShader = `
   uniform vec3 gridColor;
   uniform vec3 subGridColor;
 	uniform vec2 gridSize;
-  
   varying vec2 vUv;
 
 	void main() {
@@ -41,47 +40,43 @@ const fragmentShader = `
 }
 `;
 
-export const LinearGridShader = ({position = [0,0,0] as [number, number, number], sizeX = 100, sizeY = 100, width = 100, depth = 100}) => {
-	const rulerRef = useRef<Mesh>(null);
-  const gridConfig = useGameStore((state) => state.gridConfig);
+export const LinearGridShader = ({ position = [0, 0, 0] as [number, number, number], sizeX = 100, sizeY = 100, width = 100, depth = 100 }) => {
+  const rulerRef = useRef<Mesh>(null);
+  const gridConfig = useGameStore(state => state.gridConfig);
+
+  const { geometry, material } = useMemo(() => {
+    const planeGeometry = new PlaneGeometry(sizeX, sizeY, 1, 1);
+    const shaderMaterial = new ShaderMaterial({
+      uniforms: {
+        chunkSize: { value: gridConfig.chunkSize },
+        offset: { value: new Vector2(0, 0) },
+        subGridsX: { value: width },
+        subGridsY: { value: depth },
+        lineWidth: { value: gridConfig.lineWidth },
+        gridColor: { value: new Color(gridConfig.subGridColor) },
+        subGridColor: { value: new Color(gridConfig.subGridColor) },
+        gridSize: { value: new Vector2(sizeX, sizeY) },
+      },
+      vertexShader,
+      fragmentShader,
+      transparent: true,
+      side: DoubleSide,
+      depthWrite: false,
+    });
+    return { geometry: planeGeometry, material: shaderMaterial };
+  }, [sizeX, sizeY, width, depth, gridConfig]);
 
   useEffect(() => {
-    generateRulerGeometry();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [width, depth, sizeX, sizeY, gridConfig]);
-
-  const generateRulerGeometry = useMemo(() => {
-    const planeGeometry = new PlaneGeometry(sizeX, sizeY, 1, 1);
-		const planeMaterial = new ShaderMaterial({
-			uniforms: {
-					chunkSize: { value: gridConfig.chunkSize },
-					offset: { value: new Vector2(0, 0) },
-					subGridsX: { value: width },
-					subGridsY: { value: depth },
-					lineWidth: { value: gridConfig.lineWidth },
-					gridColor: { value: new Color(gridConfig.subGridColor) },
-					subGridColor: { value: new Color(gridConfig.subGridColor) },
-					gridSize: { value: new Vector2(sizeX, sizeY) },
-			},
-			vertexShader,
-			fragmentShader,
-			transparent: true,
-			side: DoubleSide,
-			depthWrite: false,
-	});
-
-    return () => {
-      if (rulerRef.current) {
-        rulerRef.current.geometry = planeGeometry;
-        rulerRef.current.material = planeMaterial;
-      }
-    };
-  }, [sizeX, sizeY, gridConfig.chunkSize, gridConfig.lineWidth, gridConfig.subGridColor, width, depth]);
+    if (rulerRef.current) {
+      rulerRef.current.geometry = geometry;
+      rulerRef.current.material = material;
+    }
+  }, [geometry, material]);
 
   return (
     <mesh position={position} ref={rulerRef} rotation={[-Math.PI / 2, 0, 0]}>
-      <planeGeometry />
-      <shaderMaterial />
+      <primitive attach="geometry" object={geometry} />
+      <primitive attach="material" object={material} />
     </mesh>
   );
 };
