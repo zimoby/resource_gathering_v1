bbc3935174492d1a9d44aade453f9f7daea0bf68
Denys Bondartsov <zimoby@gmail.com>
2024-05-26 16:24:25 +0300

    update eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,50 @@
+const path = require('path');
+
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    `plugin:@typescript-eslint/stylistic-type-checked`,
+    'plugin:react/recommended',
+    `plugin:react/jsx-runtime`,
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'prettier'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    project: path.resolve(__dirname, './tsconfig.json'),
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ['react-refresh', '@typescript-eslint', 'react', 'react-hooks', 'prettier'],
   rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/jsx-uses-vars': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
-    ],
+      { allowConstantExport: true }
+    ]
   },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
 }
