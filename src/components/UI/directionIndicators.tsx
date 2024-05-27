20b6a4121bec7a1a57572362a8e6791b23adf623
Denys Bondartsov <zimoby@gmail.com>
2024-05-27 04:21:45 +0300

    update ui

--- src/components/UI/directionIndicators.tsx
+++ src/components/UI/directionIndicators.tsx
@@ -10,22 +10,22 @@ export const DirectionIndicators = () => {
     <div className="absolute bottom-2 left-2 mr-1" style={{ opacity }}>
       <div className="grid grid-cols-2 gap-1">
         <div
-          className={`bg-neutral-900 size-5 border border-uilines flex justify-center items-center uppercase text-2xs ${moveDirection.x === -1 ? "bg-uilines text-neutral-900" : ""}`}
+          className={`bg-neutral-900 size-5 border border-uilines flex justify-center items-center uppercase text-2xs ${moveDirection.x === -1 ? "bg-uilines text-neutral-900" : "text-uitext"}`}
         >
           a
         </div>
         <div
-          className={`bg-neutral-900 size-5 border border-uilines flex justify-center items-center uppercase text-2xs ${moveDirection.y === -1 ? "bg-uilines text-neutral-900" : ""}`}
+          className={`bg-neutral-900 size-5 border border-uilines flex justify-center items-center uppercase text-2xs ${moveDirection.y === -1 ? "bg-uilines text-neutral-900" : "text-uitext"}`}
         >
           w
         </div>
         <div
-          className={`bg-neutral-900 size-5 border border-uilines flex justify-center items-center uppercase text-2xs ${moveDirection.y === 1 ? "bg-uilines text-neutral-900" : ""}`}
+          className={`bg-neutral-900 size-5 border border-uilines flex justify-center items-center uppercase text-2xs ${moveDirection.y === 1 ? "bg-uilines text-neutral-900" : "text-uitext"}`}
         >
           s
         </div>
         <div
-          className={`bg-neutral-900 size-5 border border-uilines flex justify-center items-center uppercase text-2xs ${moveDirection.x === 1 ? "bg-uilines text-neutral-900" : ""}`}
+          className={`bg-neutral-900 size-5 border border-uilines flex justify-center items-center uppercase text-2xs ${moveDirection.x === 1 ? "bg-uilines text-neutral-900" : "text-uitext"}`}
         >
           d
         </div>
